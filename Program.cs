e12fffb96136d5b3222ce8d9a28efae3f29e9bfb
CouchParty Games <83233322+CouchPartyGames@users.noreply.github.com>
2021-06-22 17:25:07 -0400

    adding player ordering

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Tournament;
+using System.Collections.Generic;
 
 namespace Tournament
 {
@@ -7,14 +8,48 @@ namespace Tournament
     {
         static void Main(string[] args)
         {
+            List<Opponent> opps = new List<Opponent>() {
+                new Opponent(1, "Bill"),
+                new Opponent(2, "Bob"),
+                new Opponent(3, "Steve"),
+                new Opponent(4, "Greg", 3),
+                new Opponent(5, "Jeter", 7),
+                new Opponent(6, "Carter", 9),
+                new Opponent(7, "Aaron", 2),
+                new Opponent(8, "Tom", 4),
+                new Opponent(9, "Carl", 5)
+            };
 
             Tournament tournay = new Tournament(new TournamentSettings());
-            tournay.AddOpponent(new Opponent(1, "Bill"));
-            tournay.AddOpponent(new Opponent(2, "Bob"));
-            tournay.AddOpponent(new Opponent(3, "Steve"));
+
+            foreach(Opponent opp in opps) {
+                tournay.AddOpponent(opp);
+            }
+
             //tournay.SetOpponents();
-            Console.WriteLine(tournay.Opponents.Count);
             tournay.Generate();
+
+
+            Console.WriteLine("Ranked Order");
+            OpponentOrderRank rank = new OpponentOrderRank(opps);
+            Console.WriteLine($"Num Byes: {rank.NumByes}");
+            Console.WriteLine($"Draw Size: {rank.DrawSize}");
+
+            foreach(KeyValuePair<int, Opponent> entry in rank.OpponentsInOrder) {
+                Console.WriteLine($"{entry.Key} {entry.Value.Name} {entry.Value.Rank}");
+            }
+
+
+            Console.WriteLine("");
+            OpponentOrderRandom rank2 = new OpponentOrderRandom(opps);
+            Console.WriteLine("Random Order");
+            Console.WriteLine($"Num Byes: {rank2.NumByes}");
+            Console.WriteLine($"Draw Size: {rank.DrawSize}");
+
+            foreach(KeyValuePair<int, Opponent> entry in rank2.OpponentsInOrder) {
+                Console.WriteLine($"{entry.Key} {entry.Value.Name} {entry.Value.Rank}");
+            }
+
         }
     }
 }
--- Tournament.cs
+++ Tournament.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System;
+using System.Linq;
 
 
 namespace Tournament {
@@ -13,7 +14,7 @@ namespace Tournament {
         public DateTime? StartDate { get; set; } = null;
 
             // All Opponents
-        public Dictionary<int, Opponent> Opponents { get; private set; }
+        public List<Opponent> Opponents { get; private set; }
 
             // Rounds
         public List<Round> Rounds { get; set; }
@@ -27,70 +28,24 @@ namespace Tournament {
             Settings = settings;
 
             Rounds = new List<Round>();
-            Opponents = new Dictionary<int, Opponent>();
+            Opponents = new List<Opponent>();
         }
 
-        public void SetOpponents() {
-            //Opponents = 
+        public void SetOpponents(List<Opponent> opps) {
+            Opponents = opps;
         }
 
         public void AddOpponent(Opponent opp1) {
             try {
-                Opponents.Add(opp1.Id, opp1);
+                Opponents.Add(opp1);
             } catch(ArgumentException) {
             }
         }
 
         public void Generate() {
 
-            if (Opponents.Count > 2) {
-
-                if (Opponents.Count == 2) {
-                    /*
-                    foreach(KeyValuePair<int, Opponent> entry in Opponents) {
-                        entry;
-                    }
-
-                    Match match = new Match(opp1, opp2);
-
-                    Round round = new Round();
-                    round.AddMatch(match);
-                        // Add the Round
-                    Rounds.Add(round);
-                    */
-
-                    var numRounds = 1;
-
-                } else if (Opponents.Count <= 4) {
-                    var numRounds = 2;
-
-                } else if (Opponents.Count <= 8) {
-                    var numRounds = 3;
-
-                } else if (Opponents.Count <= 16) {
-                    var numRounds = 4;
-
-                } else if (Opponents.Count <= 32) {
-                    var numRounds = 5;
-
-                } else if (Opponents.Count <= 64) {
-                    var numRounds = 6;
-
-                } else if (Opponents.Count <= 128) {
-                    var numRounds = 7;
-
-                }
-                    /* 
-                    // Setup Matches
-                Match match = new Match(opp1, opp2);
-                    // Setup the Round
-                Round round = new Round();
-                round.AddMatch(match);
-                    // Add the Round
-                Rounds.Add(round);
-                */
-            }
-
+             //var ordered = new OpponentOrderRank();
+             //var random = new OpponentOrderRandom();
         }
     }
 
@@ -122,27 +77,51 @@ namespace Tournament {
 
     public class Match : IMatch {
 
+        public enum RoundId {
+            Round128,
+            Round64,
+            Round32,
+            Round16,
+            Quarterfinals,
+            Semifinals,
+            Finals
+        }
+
         public enum MatchState {
             Ready = 0,
             InProgress,
             Completed
         }
 
+
         public int Id { get; set; }
         public MatchState State { get; set; } = MatchState.Ready;
         public Opponent Opp1 { get; private set; }
         public Opponent Opp2 { get; private set; }
         public Opponent Winner { get; set; } 
+        public RoundId Round { get; private set; }
 
 
-        public Match(Opponent opp1, Opponent opp2) {
+        public Match(int id, RoundId round) {
+            Id = id;
+            Round = round;
+        }
+
+
+        public Match(int id, RoundId round, Opponent opp1, Opponent opp2) {
+            Id = id;
+            Round = round;
+
+            SetOpponents(opp1, opp2);
+        }
+
+        public void SetOpponents(Opponent opp1, Opponent opp2) {
             Opp1 = opp1;
             Opp2 = opp2;
         }
 
 
         public void SetWinner(Opponent winner) {
-
             if (Winner == null) {
                 if (winner.Id == Opp1.Id || winner.Id == Opp2.Id) {
                     State = MatchState.Completed;
@@ -159,15 +138,37 @@ namespace Tournament {
 
     public class GroupMatch : IMatch {
 
+        public enum MatchState {
+            Ready = 0,
+            InProgress,
+            Completed
+        }
+
+        public enum RoundId {
+            Round128,
+            Round64,
+            Round32,
+            Round16,
+            Quarterfinals,
+            Semifinals,
+            Finals
+        }
+
         public int Id { get; set; }
 
         public Dictionary<int,Opponent> opponents = new Dictionary<int, Opponent>();
 
+        public MatchState State { get; set; } = MatchState.Ready;
         public int MinOpponents { get; set; } = 2;
         public int MaxOpponents { get; set; } = 4;
         public int NumWinners { get; set; } = 1;
+        public RoundId Round { get; private set; }
 
         public Dictionary<int,Opponent> winners = new Dictionary<int, Opponent>();
+
+        public GroupMatch(int id) {
+            Id = id;
+        }
 
         public bool AddOpponent(Opponent opp) {
             if (opponents.Count < MaxOpponents) {
@@ -178,29 +179,46 @@ namespace Tournament {
             return false;
         }
 
-        public void AddWinners(Opponent opp) {
-        }
 
         public void SetWinners(Dictionary<int, Opponent> opps) {
             winners = opps;
-            //this.State = State.Completed;
+            this.State = MatchState.Completed;
         }
     }
+
 
     public interface IOpponent {
         int Id { get; set; }
         string Name { get; set; }
+        int Rank { get; set; }
     }
+
 
     public class Opponent : IOpponent {
 
+        public const int ByeRank = 999999;
+        public const int NotRank = 888888;
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Rank { get; set; }
+        public bool IsBye { get; private set; }
 
-        public Opponent(int id, string name) {
+
+        public Opponent(int id, string name, int rank = NotRank) {
             Id = id;
             Name = name;
+            Rank = rank;
         }
+
+
+        public Opponent(int id, string name, bool isBye) {
+            Id = id;
+            Name = name;
+            IsBye = isBye;
+            Rank = isBye ? ByeRank : NotRank;
+        }
+
 
         public override string ToString() {
             return $"{Name} ({Id})";
@@ -234,4 +252,190 @@ namespace Tournament {
             return true;
         }
     }
+
+
+    public interface IOpponentOrder {
+        int NumberOpponents { get; set; }
+    }
+
+
+    public class OpponentOrderRandom : OpponentOrder {
+
+
+        public OpponentOrderRandom(List<Opponent> opps) : base(opps) {
+            Random rng = new Random();
+            var randomList = opps.OrderBy(a => rng.Next()).ToList();
+
+            int i = 0;
+            foreach(Opponent opp in randomList) {
+                OpponentsInOrder.Add(i, opp);
+                i++;
+            }
+
+                // Determine if Byes are needed
+            AddByeOpponents(i);
+        }
+
+    }
+
+
+    /**
+     * All Opponents are ranked in order
+     */
+    public class OpponentOrderRank : OpponentOrder {
+
+        public OpponentOrderRank(List<Opponent> opps) : base(opps) {
+            var OpponentsRanked = opps.OrderBy(o => o.Rank);
+            //var OpponentsRanked = opps.OrderByDescending(o => o.Rank);
+            int i = 0;
+            foreach(Opponent opp in OpponentsRanked) {
+                OpponentsInOrder.Add(i, opp);
+                i++;
+            }
+
+            AddByeOpponents(i);
+        }
+    }
+
+    /***
+     * Only Seeds are Ranked in Order, Everyone outside of the seeded are randomly ordered in the draw
+     */
+
+
+    /**
+     * Base Class
+     */
+    public class OpponentOrder {
+
+        public enum DrawType {
+            Draw2 = 2,
+            Draw4 = 4,
+            Draw8 = 8,
+            Draw16 = 16,
+            Draw32 = 32,
+            Draw64 = 64,
+            Draw128 = 128
+        }
+
+        public DrawType DrawSize { get; private set; }
+
+        public Dictionary<int, Opponent> OpponentsInOrder { get; private set; }
+
+        public int NumByes { get; private set; }
+
+
+        public OpponentOrder(List<Opponent> opps) {
+            OpponentsInOrder = new Dictionary<int, Opponent>();
+
+                // Set Draw Type
+            if (opps.Count <= (int)DrawType.Draw2) {
+                DrawSize = DrawType.Draw2;
+            } else if (opps.Count <= (int)DrawType.Draw4) {
+                DrawSize = DrawType.Draw4;
+            } else if (opps.Count <= (int)DrawType.Draw8) {
+                DrawSize = DrawType.Draw8;
+            } else if (opps.Count <= (int)DrawType.Draw16) {
+                DrawSize = DrawType.Draw16;
+            } else if (opps.Count <= (int)DrawType.Draw32) {
+                DrawSize = DrawType.Draw32;
+            } else if (opps.Count <= (int)DrawType.Draw64) {
+                DrawSize = DrawType.Draw64;
+            } else {
+                DrawSize = DrawType.Draw128;
+            }
+
+            NumByes = (int)DrawSize - opps.Count;
+        }
+
+
+        protected void AddByeOpponents(int startPos) {
+
+                // Determine if Byes are needed
+            if (NumByes > 0) {
+                for(int j = 0; j < NumByes; j++) {
+                    OpponentsInOrder.Add(startPos, new Opponent(0, "Bye", true));
+                    startPos++;
+                }
+            }
+        }
+    }
+
+
+    /*
+    public class MatchGenerator {
+
+        public void Generate() {
+        }
+
+        void Draw2() {
+                // #1 vs #2
+            Match match = new Match(1, OpponentsInOrder[0], OpponentsInOrder[1]);
+        }
+
+        void Draw4() {
+                // #1 vs 4
+            Match match1 = new Match(1, OpponentsInOrder[0], OpponentsInOrder[3]);
+                // #3 vs 2
+            Match match2 = new Match(1, OpponentsInOrder[2], OpponentsInOrder[1]);
+        }
+
+        void Draw8() {
+
+                // #1 vs #8
+            Match match1 = new Match(1, OpponentsInOrder[0], OpponentsInOrder[7]);
+                // #6 vs #3
+            Match match2 = new Match(1, OpponentsInOrder[5], OpponentsInOrder[2]);
+                // #4 vs #5
+            Match match3 = new Match(1, OpponentsInOrder[3], OpponentsInOrder[4]);
+                // #7 vs #2
+            Match match4 = new Match(1, OpponentsInOrder[6], OpponentsInOrder[1]);
+
+                // Quarterfinals
+            Round quarters = new Round();
+            Add(match1);
+            Add(match2);
+            Add(match3);
+            Add(match4);
+
+                // Semifinals
+            Round semis = new Round();
+            semis.Add(match5);
+            semis.Add(match6);
+
+                // Finals
+            Round finals = new Round();
+            finals.Add(match7);
+        }
+
+        void Draw16() {
+                // #1 vs #16
+            Match match1 = new Match(1, OpponentsInOrder[0], OpponentsInOrder[15]);
+            Match match2 = new Match(1, OpponentsInOrder[5], OpponentsInOrder[2]);
+            Match match3 = new Match(1, OpponentsInOrder[3], OpponentsInOrder[4]);
+            Match match4 = new Match(1, OpponentsInOrder[6], OpponentsInOrder[1]);
+        }
+
+        void Draw32() {
+            Match match1 = new Match(1, OpponentsInOrder[0], OpponentsInOrder[7]);
+            Match match2 = new Match(1, OpponentsInOrder[5], OpponentsInOrder[2]);
+            Match match3 = new Match(1, OpponentsInOrder[3], OpponentsInOrder[4]);
+            Match match4 = new Match(1, OpponentsInOrder[6], OpponentsInOrder[1]);
+        }
+
+
+        void Draw64() {
+            Match match1 = new Match(1, OpponentsInOrder[0], OpponentsInOrder[7]);
+            Match match2 = new Match(1, OpponentsInOrder[5], OpponentsInOrder[2]);
+            Match match3 = new Match(1, OpponentsInOrder[3], OpponentsInOrder[4]);
+            Match match4 = new Match(1, OpponentsInOrder[6], OpponentsInOrder[1]);
+        }
+
+        void Draw128() {
+            Match match1 = new Match(1, OpponentsInOrder[0], OpponentsInOrder[7]);
+            Match match2 = new Match(1, OpponentsInOrder[5], OpponentsInOrder[2]);
+            Match match3 = new Match(1, OpponentsInOrder[3], OpponentsInOrder[4]);
+            Match match4 = new Match(1, OpponentsInOrder[6], OpponentsInOrder[1]);
+        }
+    }*/
+
 }
